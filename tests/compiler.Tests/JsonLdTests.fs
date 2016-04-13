07abbbbcfcda6750052f0a7821350a3b63875df4
James Kirk <james.kirk84@gmail.com>
2016-04-13 21:48:50 +0000

    json ld test now working

--- tests/compiler.Tests/JsonLdTests.fs
+++ tests/compiler.Tests/JsonLdTests.fs
@@ -3,23 +3,26 @@ module publish.Test.JsonLdTests
 open NUnit.Framework
 open Swensen.Unquote
 open publish.JsonLd
+open FSharp.Data
 open FSharp.RDF
 open Assertion
 open resource
 open rdf
 
+type JsonLdSchema = JsonProvider<""" { "http://ld.nice.org.uk/ns/qualitystandard#title":"" } """>
+
 [<Test>]
-let ``Should convert rdf resource to json-ld``() =
+let ``Should convert simple rdf resource to jsonld with context``() =
 
   let r =
     resource !! "http://ld.nice.org.uk/qualitystatement/id_goes_here" 
-      [a !! "owl:NamedIndividual"
-       dataProperty !!"http://ld.nice.org.uk/ns/qualitystandard#title" ("title goes here"^^xsd.string)]
+      [dataProperty !!"http://ld.nice.org.uk/ns/qualitystandard#title" ("title goes here"^^xsd.string)]
 
   let contexts = [
       "http://ld.nice.org.uk/ns/qualitystandard.jsonld "
     ]
 
-  let json = transformToJsonLD [[r]] contexts 
+  let json = transformToJsonLD [[r]] contexts |> Seq.head
+  let jsonld = JsonLdSchema.Parse(json)
 
-  test <@ json |> Seq.head = "hahaha" @>
+  test <@ jsonld.HttpLdNiceOrgUkNsQualitystandardTitle.JsonValue.AsString() = "title goes here" @>
