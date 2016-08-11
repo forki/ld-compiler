a5aef14d1baeeffbb0daf366d02261fa0f5e69ab
James Kirk <james.kirk84@gmail.com>
2016-08-11 11:40:57 +0100

    integration tests now run on all platforms

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -2,12 +2,12 @@
 
 docker-compose -f docker-compose.test.yaml up -d
 sleep 10
-docker-compose -f docker-compose.test.yaml run --rm integrationtests bash /compiler-src/tests/compiler.IntegrationTests/run.sh
+docker exec -it ldcompiler_integrationtests bash /compiler-src/tests/compiler.IntegrationTests/run.sh
 result=$?
 docker-compose -f docker-compose.test.yaml stop
 if [ $result -ne 0 ]; then
    echo "Tests failed, showing logs from compiler container"
-   docker logs ldcompiler_compiler_1
+   docker-compose -f docker-compose.test.yaml logs compiler
 fi
 docker-compose -f docker-compose.test.yaml rm -vf
 exit $result
