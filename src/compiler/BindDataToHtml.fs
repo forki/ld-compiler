eb998204bbddd680d9390943a2d620ee80647c80
David Wong <wongcheming@gmail.com>
2016-09-02 09:53:37 +0100

    refactored dot liquid template extensions to a reusable place

--- src/compiler/BindDataToHtml.fs
+++ src/compiler/BindDataToHtml.fs
@@ -1,44 +1,27 @@
 module compiler.BindDataToHtml
 
-open compiler.Domain
 open System
 open System.IO
-open DotLiquid
-open Microsoft.FSharp.Reflection
 
-let parseTemplate<'T> template =
-  let rec registerTypeTree ty =
-    if FSharpType.IsRecord ty then
-      let fields = FSharpType.GetRecordFields(ty)
-      Template.RegisterSafeType(ty, [| for f in fields -> f.Name |])
-      for f in fields do registerTypeTree f.PropertyType
-    elif ty.IsGenericType &&
-        ( let t = ty.GetGenericTypeDefinition()
-          in t = typedefof<seq<_>> || t = typedefof<list<_>> ) then
-      () //registerTypeTree (ty.GetGenericArguments().[0])
-      registerTypeTree (ty.GetGenericArguments().[0])
-    else () (* printfn "%s" ty.FullName *)
+open compiler.Domain
+open compiler.DotLiquidExtensions
 
-  registerTypeTree typeof<'T>
-  let t = Template.Parse(template)
-  fun k (v:'T) -> t.Render(Hash.FromDictionary(dict [k, box v]))
-
-type Annot =
+type Metadata =
   {
-  FirstIssued: string 
+    FirstIssued: string 
   }
 
 let bindDataToHtml statement =
   let firstIssued = statement.Annotations |> List.find (fun x -> x.Vocab.Replace(" ","").ToLower() = "firstissued") 
 
 
-  let t = parseTemplate<Annot> """
-<table>
+  let metadataTable = parseTemplate<Metadata> """
+<table id="metadata">
 <tr>
-<td>First Issued On</td><td>{{annot.first_issued |  date: "MMMM yyyy" }}</td>
+<td>First issued on</td><td>{{metadata.first_issued |  date: "MMMM yyyy" }}</td>
 </tr>
 </table>
 """
 
-  let newHtml = t  "annot" { FirstIssued=firstIssued.Terms.Head} 
+  let newHtml = metadataTable  "metadata" { FirstIssued=firstIssued.Terms.Head} 
   { statement with Html = newHtml }
