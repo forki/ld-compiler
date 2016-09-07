d87b54d868c410e1202b5f0d6234191399cc0e61
Stewart Ainsworth <stewart.ainsworth@nice.org.uk>
2016-09-07 16:37:08 +0100

    tidied up code a little

--- src/compiler/ValidationUtils.fs
+++ src/compiler/ValidationUtils.fs
@@ -12,139 +12,13 @@ let private raiseError annotation state =
   | _ -> sprintf "Error (%s) encountered while processing the '%s' annotation" state annotation
   |> failwith
 
-let private processDate name field =
-  let validateDate (date:string) (raiseError:string -> string) =
-    if (obj.ReferenceEquals(date, null)=false && date.Length > 0) then
-      match System.DateTime.TryParseExact(date, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture,System.Globalization.DateTimeStyles.None) with
-      | true, x -> x.ToString("yyyy-MM-dd")
-      | _ ->  raiseError "Invalid"
-    else
-      raiseError "Missing"
-  let raiseDateError = raiseError name
-
-  validateDate field raiseDateError
-
-let private validatePositionalId (posnId:string) =
-  let posnIdError = raiseError "PositionalId"
-
-  let valid (prefix:string) (part:string) =
-    let compare = sprintf "%s%s" prefix (part.Replace(prefix,""))
-    System.String.Equals(compare, part)
-
-  let validateParts qs st =
-    match (valid "qs" qs) && (valid "st" st) with
-    | true -> sprintf "%s-%s" qs st
-    | _ -> posnIdError "Invalid"
-
-  let idParts = posnId.Split [|'-'|] |> Array.toList
-
-  match idParts.Length with
-  | 2 -> validateParts (idParts |> List.head) (idParts |> List.tail |> List.head)
-  | _ -> posnIdError "Invalid"
-
-let private validateYesNo name value =
-  match value with
-  | "yes"
-  | "no" -> value
-  | _ -> raiseError name "Invalid"
-
-let private validateValue validation (value:string) =
-  let matchValidationType validation format value =
-    match format with
-    | "Date" -> processDate validation.Label value
-    | "PositionalId" -> validatePositionalId value
-    | "YesNo" -> validateYesNo validation.Label value
-    | _ -> value
-
-  let matchValidationHead value (formatOption:string Option) =
-      match formatOption.IsNone with
-      | true -> value
-      | false -> matchValidationType validation formatOption.Value value
-
-  match obj.ReferenceEquals(validation.Format, null) with
-  | true -> value
-  | _ ->  validation.Format.Split [|':'|] |> Array.tryHead |> matchValidationHead value
-
-let private validateMandatoryAnnotations validations annotations = 
-  let validateAnnotationExists annotations mandatoryValidation =
-
-    let a = annotations |> List.filter (fun a -> a.Vocab = mandatoryValidation.Label)
-      
-    match a.Length with
-    | 0 -> raiseError mandatoryValidation.Label "Missing"
-    | _ -> match a.Head.Vocab.Length with
-           | 0 -> raiseError mandatoryValidation.Label "Blank"
-           | _ -> ()
- 
-  let assessAnnotations (validationParts:string array) =
-    let assessTerms annotationTerms thisTerm =
-      annotationTerms |> List.filter (fun t -> t = thisTerm)
-                      |> List.length > 0
-
-    let annotationVocab = Array.get validationParts 2
-    let annotationTerm = Array.get validationParts 3
-    annotations |> List.filter (fun a -> a.Vocab = annotationVocab)
-                |> List.map (fun a -> assessTerms a.Terms annotationTerm)
-                |> List.contains true
- 
-  let shouldProcessValidation validation =
-    let validationParts = validation.Format.Split [|':'|]
-    match validationParts.Length with
-    | 2 -> match Array.get validationParts 1 with
-           | "Required" -> validation, true
-           | _ -> validation, false
-    | 4 -> match Array.get validationParts 1 with
-           | "Conditional" -> validation, (assessAnnotations validationParts)
-           | _ -> validation, false
-    | _ -> validation, false
-
-  let activeValidation validation =
-    match obj.ReferenceEquals(validation.Format, null) with
-    | false -> shouldProcessValidation validation
-    | _ -> validation, false
-
-  validations |> List.map (fun v -> activeValidation v)
-              |> List.filter (fun t -> snd t)
-              |> List.map (fun t -> fst t)
-              |> List.map (fun v -> validateAnnotationExists annotations v)
-              |> ignore
-  annotations
-
-let private validateProvidedAnnotations validations annotations =
-  let validateAnnotation validations (thisAnnotation:Annotation) =
-    let relevantValidation = validations |> List.filter (fun v -> v.Label = thisAnnotation.Vocab)
-    match relevantValidation.Length with
-    | 0 -> thisAnnotation
-    | _ -> { thisAnnotation with Terms = thisAnnotation.Terms |> List.map (fun t -> validateValue relevantValidation.Head t) }
-
-  annotations |> List.map (fun a -> validateAnnotation validations a)
-
-//let validateStatement validations (statement:Statement) =
-//  {
-//    Id = statement.Id
-//    Title = statement.Title
-//    Abstract = statement.Abstract
-//    StandardId = statement.StandardId
-//    StatementId = statement.StatementId
-//    ObjectAnnotations = statement.ObjectAnnotations
-//                        |> List.filter (fun x -> x.Terms.Length > 0)
-//    DataAnnotations = statement.DataAnnotations
-//                      |> List.filter (fun x -> x.Terms.Length > 0)
-//                      |> validateMandatoryAnnotations validations
-//                      |> validateProvidedAnnotations validations
-//    Content = statement.Content
-//    Html = statement.Html
-//  }
-
-///////////////////////////////// this is the new shit //////////////////////////////////////////////
-
-let verifyRequiredAnnotationsExist (theseValidations:PublishItem List) (theseAnnotations:Annotation List) =
+let verifyRequiredAnnotationsExist theseValidations theseAnnotations =
   let validationHasFormat thisValidation =
     match obj.ReferenceEquals(thisValidation.Format, null) with
     | false -> thisValidation, true
     | _ -> thisValidation, false
 
-  let isConditionalReqires (validationParts:string array) = 
+  let isConditionalReqires validationParts = 
     let condVocab = Array.get validationParts 2
     let condTerm = Array.get validationParts 3
     theseAnnotations
@@ -154,7 +28,7 @@ let verifyRequiredAnnotationsExist (theseValidations:PublishItem List) (theseAnn
                  | 0 -> false
                  | _ -> true
 
-  let isValidationRequired (thisValidation:PublishItem) =
+  let isValidationRequired thisValidation =
     let validationParts = thisValidation.Format.Split [|':'|]
     match validationParts.Length with
     | 0 
@@ -164,7 +38,7 @@ let verifyRequiredAnnotationsExist (theseValidations:PublishItem List) (theseAnn
            | "Conditional" -> thisValidation, (isConditionalReqires validationParts)
            | _ -> thisValidation, false
 
-  let isRequiresAnnotationInList (thisValidation:PublishItem) =
+  let isRequiresAnnotationInList thisValidation =
     let foundAnnotations = theseAnnotations |> List.filter (fun a -> a.Vocab = thisValidation.Label)
     match foundAnnotations.Length with
     | 0 -> raiseError thisValidation.Label "Missing"
@@ -182,71 +56,62 @@ let verifyRequiredAnnotationsExist (theseValidations:PublishItem List) (theseAnn
 
   theseAnnotations
 
-//  let private processDate name field =
-//  let validateDate (date:string) (raiseError:string -> string) =
-//    if (obj.ReferenceEquals(date, null)=false && date.Length > 0) then
-//      match System.DateTime.TryParseExact(date, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture,System.Globalization.DateTimeStyles.None) with
-//      | true, x -> x.ToString("yyyy-MM-dd")
-//      | _ ->  raiseError "Invalid"
-//    else
-//      raiseError "Missing"
-//  let raiseDateError = raiseError name
-//  validateDate field raiseDateError
-let private validateDataAnnotation (thisAnnotation:Annotation) =
-  let processDates () =
-    let tryparseDate (date:string) = 
-      match System.DateTime.TryParseExact(date, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture,System.Globalization.DateTimeStyles.None) with
-      | true, x -> x.ToString("yyyy-MM-dd")
-      | _ ->  raiseError thisAnnotation.Vocab "Invalid"
+let private processDates thisAnnotation =
+  let tryparseDate date = 
+    match System.DateTime.TryParseExact(date, "dd-MM-yyyy", System.Globalization.CultureInfo.InvariantCulture,System.Globalization.DateTimeStyles.None) with
+    | true, x -> x.ToString("yyyy-MM-dd")
+    | _ ->  raiseError thisAnnotation.Vocab "Invalid"
     
-    let processedTerms = thisAnnotation.Terms
+  let processedTerms = thisAnnotation.Terms
                          |> List.map (fun t -> tryparseDate t)
-    { thisAnnotation with Terms = processedTerms}
+  { thisAnnotation with Terms = processedTerms}
  
-  let processPositionalId () =
-    let posnIdError = raiseError "PositionalId"
+let private processPositionalId thisAnnotation =
+  let posnIdError = raiseError "PositionalId"
 
-    let valid (prefix:string) (part:string) =
-      let compare = sprintf "%s%s" prefix (part.Replace(prefix,""))
-      System.String.Equals(compare, part)
+  let valid prefix (part:string) =
+    let compare = sprintf "%s%s" prefix (part.Replace(prefix,""))
+    System.String.Equals(compare, part)
 
-    let validateParts qs st =
-      match (valid "qs" qs) && (valid "st" st) with
-      | true -> ()
-      | _ -> posnIdError "Invalid"
-
-    let splitAndProcessPositionalId (positionalId:string) =
-      let idParts = positionalId.Split [|'-'|] |> Array.toList
-
-      match idParts.Length with
-      | 2 -> validateParts (idParts |> List.head) (idParts |> List.tail |> List.head)
-      | _ -> posnIdError "Invalid"
-         
-    match thisAnnotation.Terms.Length with
-    | 1 -> thisAnnotation.Terms.Head |> splitAndProcessPositionalId
+  let validateParts qs st =
+    match (valid "qs" qs) && (valid "st" st) with
+    | true -> ()
     | _ -> posnIdError "Invalid"
+
+  let splitAndProcessPositionalId (positionalId:string) =
+    let idParts = positionalId.Split [|'-'|] |> Array.toList
+
+    match idParts.Length with
+    | 2 -> validateParts (idParts |> List.head) (idParts |> List.tail |> List.head)
+    | _ -> posnIdError "Invalid"
+         
+  match thisAnnotation.Terms.Length with
+  | 1 -> thisAnnotation.Terms.Head |> splitAndProcessPositionalId
+  | _ -> posnIdError "Invalid"
     
-    thisAnnotation
+  thisAnnotation
 
-  let processYesNo () =
-    let assessTerm (term:string) =
-      match term with
-      | "yes"
-      | "no" -> ()
-      | _ -> raiseError thisAnnotation.Vocab "Invalid"
+let private processYesNo thisAnnotation =
+  let assessTerm term =
+    match term with
+    | "yes"
+    | "no" -> ()
+    | _ -> raiseError thisAnnotation.Vocab "Invalid"
 
-    thisAnnotation.Terms
-    |> List.map (fun t -> assessTerm t)
+  thisAnnotation.Terms
+  |> List.map (fun t -> assessTerm t)
+  |> ignore
 
-    thisAnnotation
+  thisAnnotation
 
+let private validateDataAnnotation (thisAnnotation:Annotation) =
   let validationParts = thisAnnotation.Format.Split [|':'|]   
   match validationParts.Length with
     | 0 -> thisAnnotation
     | _ -> match Array.get validationParts 0 with
-           | "Date" -> processDates ()
-           | "PositionalId" -> processPositionalId () 
-           | "YesNo" -> processYesNo ()
+           | "Date" -> processDates thisAnnotation
+           | "PositionalId" -> processPositionalId thisAnnotation
+           | "YesNo" -> processYesNo thisAnnotation
            | _ -> thisAnnotation
 
 let validateStatement (statement:Statement) =
