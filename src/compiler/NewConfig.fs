6a5aa33a869908a51de472f705a30aaa4d7e3287
James Kirk <james.kirk84@gmail.com>
2016-09-20 15:56:06 +0100

    first test schema base

---
+++ src/compiler/NewConfig.fs
@@ -0,0 +1,13 @@
+﻿module compiler.NewConfig
+
+open Newtonsoft.Json
+
+type Config = {
+  SchemaBase: string
+}
+
+let createConfig jsonString = 
+  let deserialisedConfig = JsonConvert.DeserializeObject<ConfigTypes.Config>(jsonString)
+
+  {SchemaBase = deserialisedConfig.SchemaBase}
+
