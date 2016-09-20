4b8f3fc1f71f50078dfd6e16858a1457f41b2dd5
James Kirk <james.kirk84@gmail.com>
2016-09-20 15:56:06 +0100

    first test schema base

---
+++ tests/compiler.Tests/NewConfigTests.fs
@@ -0,0 +1,173 @@
+﻿module NewConfigTests
+
+open NUnit.Framework
+open FsUnit
+open compiler.NewConfig
+
+let private sampleConfig = """
+{
+	"SchemaBase": "http://schema/ns/",
+	"UrlBase": "http://ld.nice.org.uk/",
+	"QSBase": "ns/qualitystandard",
+	"ThingBase": "resource",
+	"IndexName": "kb",
+	"TypeName": "qualitystatement",
+	"SchemaDetails":
+	[
+		{
+			"Schema": "qualitystandard/setting.ttl",
+			"JsonLD": "qualitystandard/setting.jsonld ",
+			"Map": true,
+			"Publish":
+			[
+				{
+					"Uri": "setting",
+					"PropertyPath":
+					[
+						"^rdfs:subClassOf*"
+					]
+				}
+			]
+		},
+		{
+			"Schema": "qualitystandard/agegroup.ttl",
+			"JsonLD": "qualitystandard/agegroup.jsonld ",
+			"Map": true,
+			"Publish":
+			[
+				{
+					"Uri": "age",
+					"Label": "Age Group",
+					"PropertyPath":
+					[
+						"^rdfs:subClassOf*",
+						"rdfs:subClassOf*"
+					]
+				}
+			]
+			
+		},
+		{
+			"Schema": "qualitystandard/conditionordisease.ttl",
+			"JsonLD": "qualitystandard/conditionordisease.jsonld ",
+			"Map": true,
+			"Publish":
+			[
+				{
+					"Uri": "condition",
+					"Label": "Condition Or Disease",
+					"PropertyPath": 
+					[
+						"^rdfs:subClassOf*",
+						"rdfs:subClassOf*"
+					]
+				}
+			]
+			
+		},
+		{
+			"Schema": "qualitystandard/servicearea.ttl",
+			"JsonLD": "qualitystandard/servicearea.jsonld ",
+			"Map": true,
+			"Publish":
+			[
+				{
+					"Uri": "servicearea",
+					"PropertyPath": 
+					[
+						"^rdfs:subClassOf*"
+					]
+				}
+			]
+			
+		},
+		{
+			"Schema": "qualitystandard/lifestylecondition.ttl",
+			"JsonLD": "qualitystandard/lifestylecondition.jsonld ",
+			"Map": true,
+			"Publish":
+			[
+				{
+					"Uri": "lifestylecondition",
+					"PropertyPath":
+					[
+						"^rdfs:subClassOf*"
+					]
+				}
+			]
+			
+		},
+		{
+			"Schema": "qualitystandard.ttl",
+			"JsonLD": "qualitystandard.jsonld ",
+			"Map": false,
+			"Publish":
+			[
+				{
+					"Uri": "title"
+				},
+				{
+					"Uri": "abstract"
+				},
+				{
+					"Uri": "qsidentifier"
+				},
+				{
+					"Uri": "stidentifier"
+				},
+				{
+					"Uri": "hasPositionalId",
+					"Label": "PositionalId",
+					"Validate": true,
+					"Format": "PositionalId:Required",
+                    "Display": {},
+                    "DataAnnotation": true,
+                    "PropertyPath": []
+				},
+				{
+					"Uri": "isNationalPriority",
+					"Label": "National priority",
+					"Validate": true,
+					"Format": "YesNo:Required",
+                    "Display": {},
+                    "DataAnnotation": true,
+                    "UndiscoverableWhen": "no",
+                    "PropertyPath": []
+				},
+				{
+					"Uri": "changedPriorityOn",
+					"Label": "Changed Priority On",
+					"Validate": true,
+					"Format": "Date:Conditional:National priority:no",
+                    "Display": {
+                        "Condition": "National priority:no",
+                        "Label": "Priority",
+                        "Template": "In {{value |  date: \"MMMM yyyy\" }} the priority of this statement changed. It is no longer considered a national priority for improvement but may still be useful at a local level."
+                    },
+                    "DataAnnotation": true,
+                    "PropertyPath": []
+				},
+				{
+					"Uri": "wasFirstIssuedOn",
+					"Label": "First issued",
+					"Validate": true,
+					"Format": "Date:Required",
+                    "Display": {
+                        "Always": true
+                    },
+                    "DataAnnotation": true,
+                    "PropertyPath": []
+				}
+			]
+			
+		}
+	]
+
+}
+"""
+
+[<Test>]
+let ``NewConfigTests: should extract schema base from config`` () =
+  let newConfig = createConfig sampleConfig
+  newConfig.SchemaBase |> should equal "http://schema/ns/"
+
